d6d4571f920b28e8220e977ab75353d0160b5be8
Fabio Akira <47678698+fabioakr@users.noreply.github.com>
2022-07-09 14:10:18 -0300

    Fix da função que gera valores exatos

--- EP3.py
+++ EP3.py
@@ -11,18 +11,23 @@ import numpy as np
 ##  Funcao principal.  ##
 def main():
     ##  Escolha do n desejado para os calculos subsequentes.  ##
-    n = int(input("Digite o n para as analises: "))
-    L = int(input("Digite o L para as analises: ")) ## L = 1 para comecar
+    #n = int(input("Digite o n para as analises: "))
+    #L = int(input("Digite o L para as analises: ")) ## L = 1 para comecar
+    n = 5
+    L = 1
     funcao = "12*x*(1-x)-2"
 
     ##  Cálculo do intervalo entre pontos.  ##
     h = L/(n+1)
+    print("\nIntervalo entre pontos: "+str(h))
 
     ##  Criacao do vetor com valores x_i.  ##
-    x = np.zeros(n+1)
-    for i in range(0, n+1):
+    x = np.zeros(n+2)
+    for i in range(0, n+2):
         x[i] = i*h
-        print(x[i])
+        ##print(x[i])
+    print("Valores do vetor x:")
+    print(x)
 
     ## Criacao da matriz A da equacao (8)
     diagA = np.zeros(n)
@@ -36,48 +41,49 @@ def main():
 
     ## Calculo do vetor da funcao chapeu da equacao (8)
     b = np.zeros(n)
-    for i in range(1, n): # n sei se eh ate n ou n+1
+    for i in range(0, n): # COMECA EM ZERO E VAI ATE N-1
         b[i] = calculaIntegral(x[i-1],x[i],"("+funcao+")*(x-"+str(x[i-1])+")/"+str(h)) + calculaIntegral(x[i],x[i+1],  "("+str(x[i+1])+"-x)/"+str(h)) 
 
+    print("Vetor b: ")
     print(b)
 
+    ##  Resolve sistema matricial para achar o vetor alfa.  ##
     vetorAlfa = resolveTridiagonal(n,diagA,diagB,diagC,b)
+    print("vetor alfa resolvido: ")
+    print(vetorAlfa)
 
-    resposta = np.zeros(n)
+    #olaaa = "+3"
+    #print(calculaIntegral(-1,2,"("+"pow(x,2)"+")"+olaaa))
+    #print(calculaIntegralNDois(-1,2,"("+"pow(x,2)"+")"+olaaa))
+    #olaaa = "+1"
+    #print(calculaIntegralNDois(0,2,"("+"3*pow(x,2)+6*x"+")"+olaaa))
+    #print(calculaIntegral(0,2,"("+"3*pow(x,2)+6*x"+")"+olaaa))
 
-    ## to achando que esses for aqui tao errados K
-    for i in range(1, n):
-        for j in range(1, n):
-            resposta[i] = resposta[i] + vetorAlfa[j] * (x[j+1]-x[j-1])/h
+    #print(geraVetorValidacao(5,x))
 
-    print(resposta)
+    #print("Para u = 1, valor esperado: "+str((0.5)^2+(0.5)^2))
+    #print(funcaoTeste(5,0,1,x))
+    print(geraVetorValidacao(5,x))
 
-    olaaa = "+3"
-    print(calculaIntegral(-1,2,"("+"pow(x,2)"+")"+olaaa))
-    print(calculaIntegralNDois(-1,2,"("+"pow(x,2)"+")"+olaaa))
-    olaaa = "+1"
-    print(calculaIntegralNDois(0,2,"("+"3*pow(x,2)+6*x"+")"+olaaa))
-    print(calculaIntegral(0,2,"("+"3*pow(x,2)+6*x"+")"+olaaa))
-
-
-    #print(geraVetorValidacao())
-
-def calculaVetorb(n,funcao,):
-    print(2)
+    # resposta para um x especifico
+    valorxespec = 0.5
+    #for i in range (1,n+1):
+        
 
 ##  Funcao que gera o vetor de validacao do item 4.2.  ##
-def geraVetorValidacao(n,vetorX):
-    sol = np.zeros(n)
-    for i in range (0,n):
-        sol[n] = (vetorX[n])^2 + (1 - vetorX[n])^2
-    return sol
+def geraVetorValidacao(n,x):
+    valoresExatos = np.zeros(n)
 
-##  Funcao que gera o vetor de validacao do complemento do item 4.2.  ##
-def geraVetorValidacaoAdicional(n,vetorX):
-    sol = np.zeros(n)
     for i in range (0,n):
-        sol[n] = (vetorX[n] - 1) * (np.exp(-vetorX[n]) - 1)
-    return sol
+        valoresExatos[i] = pow(x[i],2) * pow(1 - x[i],2)
+    return valoresExatos
+
+def geraVetorValidacaoProblemaDois(n,x):
+    valoresExatos = np.zeros(n)
+    
+    for i in range (0,n):
+        valoresExatos[i] = pow(x[i],2) + pow(1 - x[i],2)
+    return valoresExatos
 
 ##  Funcao que calcula integral simples em dx (n = 10).          ##
 ##  OBS: funcao NOVA, mas derivada do EP2                        ##
