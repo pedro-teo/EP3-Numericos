01767693c627a3d0e83c43dc30df0fb83ba0a79c
fabioakr <47678698+fabioakr@users.noreply.github.com>
2022-07-09 14:18:15 -0300

    Alteracoes no rascunho p/ tirar warnings

--- arquivoaux.py
+++ arquivoaux.py
@@ -1,20 +1,28 @@
+# cabecalho pra n dar warnings
+import numpy as np
+n = 54
+x = np.zeros(n)
+h = 3434
+vetorAlfa = np.zeros(23)
+# fim do cabecalho inutil
+
 ## to achando que esses for aqui tao errados K
-    resposta = np.zeros(n+2)
-    for i in range(0, n+1):
-        print(x[i])
-        for j in range(1, n+1):
-            print("par:")
-            print(x[j-1])
-            print(x[j])
-            print(x[j+1])
-            print()
-            if  (x[i] >= x[j-1] and x[i] <= x[j]):
-                resposta[i] = resposta[i] + vetorAlfa[j] * (x[i]-x[j-1])/h
-            elif(x[i] <= x[j+1] and x[i] >= x[j]):
-                resposta[i] = resposta[i] + vetorAlfa[j] * (x[j+1]-x[i])/h
-            else:
-                #qalo
-                resposta[i] = resposta[i] + 0
+resposta = np.zeros(n+2)
+for i in range(0, n+1):
+    print(x[i])
+    for j in range(1, n+1):
+        print("par:")
+        print(x[j-1])
+        print(x[j])
+        print(x[j+1])
+        print()
+        if  (x[i] >= x[j-1] and x[i] <= x[j]):
+            resposta[i] = resposta[i] + vetorAlfa[j] * (x[i]-x[j-1])/h
+        elif(x[i] <= x[j+1] and x[i] >= x[j]):
+            resposta[i] = resposta[i] + vetorAlfa[j] * (x[j+1]-x[i])/h
+        else:
+            #qalo
+            resposta[i] = resposta[i] + 0
     print(resposta)
 
     # comentarios para teste de calculo de integral
