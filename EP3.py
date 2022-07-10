1f31cdf757f16352e804d0bcbf44496c8bd84ddc
fabioakr <47678698+fabioakr@users.noreply.github.com>
2022-07-09 23:24:12 -0300

    Implementacao do MEF numa funcao dedicada

--- EP3.py
+++ EP3.py
@@ -18,6 +18,7 @@ def main():
     funcao = "12*x*(1-x)-2"
 
     k = "1"
+    q = "0"
 
     ##  Cálculo do intervalo entre pontos.  ##
     h = L/(n+1)
@@ -82,8 +83,79 @@ def main():
     print("Vetor com valores aproximados:\n", vetoruAproximado)
     print("Vetor com valores exatos:\n", vetoruExato)
 
-    print(vetorAlfa - geraVetorValidacao(n,x))
+    print("ALOO")
+    print(calculaMEF(n, L, funcao, '1', 0))
 
+    #print(vetorAlfa - geraVetorValidacao(n,x))
+
+def calculaMEF(n, L, funcaoX, funcaoK, funcaoQ):
+    ##  Cálculo do intervalo entre pontos.  ##
+    h = L/(n+1)
+    print("\nIntervalo entre pontos: "+str(h))
+
+    ##  Criacao do vetor com valores x_i.  ##
+    x = np.zeros(n+2)
+    for i in range(0, n+2):
+        x[i] = i*h
+    print("Valores do vetor x:")
+    print(x)
+
+    ##  Criacao da matriz A (tridiagonal) da equacao (8), representada em três vetores.  ##
+    diagA = np.zeros(n)
+    diagB = np.zeros(n)
+    diagC = np.zeros(n)
+
+    ##  Calculo da diagonal principal.  ##
+    for i in range (0, n):
+        diagB[i] = pow((1/h),2) * calculaIntegral(x[i], x[i+1], funcaoK) + pow((-1/h),2)*calculaIntegral(x[i+1], x[i+2], funcaoK)
+
+    ##  Calculo da diagonal acima da diagonal principal.  ##
+    for i in range (0, n-1):
+        diagC[i] = -1*pow(1/h,2) * calculaIntegral(x[i], x[i+1], funcaoK)
+
+    ##  Calculo da diagonal abaixo da diagonal principal.  ##
+    for i in range (1, n):
+        diagA[i] = -1*pow(1/h,2) * calculaIntegral(x[i-1], x[i], funcaoK)
+
+    ##  Imprime as três diagonais criadas acima.  ##
+    print("a")
+    print(diagA)
+    print("b")
+    print(diagB)
+    print("c")
+    print(diagC)
+
+    ##  Calculo do vetor b, da funcao chapeu da equacao (8).  ##
+    b = np.zeros(n)
+    for i in range(0, n):
+        b[i] = calculaIntegral(x[i],x[i+1],"("+funcaoX+")*(x-"+str(x[i])+")/"+str(h)) + calculaIntegral(x[i+1],x[i+2],"("+funcaoX+")*("+str(x[i+2])+"-x)/"+str(h))
+
+    ##  Imprime o vetor b, calculado acima.  ##
+    print("Vetor b: ")
+    print(b)
+
+    ##  Resolve sistema matricial para achar o vetor alfa.  ##
+    vetorAlfa = resolveTridiagonal(n,diagA,diagB,diagC,b)
+
+    ##  Imprime o vetor alfa calculado acima.  ##
+    print("vetor alfa resolvido: ")
+    print(vetorAlfa)
+
+    ##  Finalmente, calculo do valor aproximado.  ##
+    vetoruExato      = np.zeros(n+2)
+    vetoruAproximado = np.zeros(n+2)
+
+    for j in range(0,n+2):
+        rAproximado = 0
+        for i in range(0, n):
+            if (x[j] <= x[i+1] and x[j] >= x[i]):
+                rAproximado = rAproximado + vetorAlfa[i] * (x[j] - x[i]) / h
+            elif (x[j] <= x[i+2] and x[j] >= x[i+1]):
+                rAproximado = rAproximado + vetorAlfa[i] * (x[i+2] - x[j]) / h
+        vetoruAproximado[j] = rAproximado
+        vetoruExato[j] =  pow(x[j],2) * pow((1 - x[j]),2)
+
+    return vetoruAproximado
 
 ##  Funcao que gera o vetor de validacao do item 4.2.  ##
 def geraVetorValidacao(n,x):
