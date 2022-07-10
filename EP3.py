9b21fc106a0d2474ca700c6c733115469ab23245
fabioakr <47678698+fabioakr@users.noreply.github.com>
2022-07-10 00:31:50 -0300

    Adiciona funcao adicional do PDF

--- EP3.py
+++ EP3.py
@@ -86,10 +86,9 @@ def main():
     print("ALOO")
     print(calculaMEF(n, L, funcao, '1', 0))
 
-    print(calculaMEF(n, L,"(np.exp(x)+1)", "(np.exp(x))", 0))
-    print(geraVetorValidacaoProblemaDois(n,x))
+    print(calculaMEF(n, L,"np.exp(x)+1", "np.exp(x)", 0))
 
-    #print(vetorAlfa - geraVetorValidacao(n,x))
+    print(geraVetorValidacaoProblemaDois(n,x))
 
 def calculaMEF(n, L, funcaoX, funcaoK, funcaoQ):
     ##  Cálculo do intervalo entre pontos.  ##
