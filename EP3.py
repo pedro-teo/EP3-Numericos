da4a8a85cd32a962f12570b6f0d2a4bf9f6107a0
fabioakr <47678698+fabioakr@users.noreply.github.com>
2022-07-11 09:29:06 -0300

    Implementa funcao de teste para o ex 4.4

--- EP3.py
+++ EP3.py
@@ -248,6 +248,92 @@ def calculaErroMaximo(n, vetoruAproximado, vetoruExato):
             erroMaximo = diferencaModulo
     return erroMaximo
 
+def calculaMEFteste(n, L, d, funcaoX, funcaoKs, funcaoKa, funcaoQ): #antes era so funcaoK, e n tinha d
+    ##  Calculo do limite entre materiais.  ##
+    limInferior = (L/2) - d
+    limSuperior = (L/2) + d 
+    
+    ##  Calculo do intervalo entre pontos.  ##
+    h = L/(n+1)
+    print("     Intervalo entre pontos: "+str(h))
+
+    ##  Criacao do vetor com valores x_i.  ##
+    x = np.zeros(n+2)
+    for i in range(0, n+2):
+        x[i] = i*h
+    print("     Valores do vetor x:")
+    print(x)
+
+    ##  Criacao da matriz A (tridiagonal) da equacao (8), representada em três vetores.  ##
+    diagA = np.zeros(n)
+    diagB = np.zeros(n)
+    diagC = np.zeros(n)
+
+    ##  Calculo da diagonal principal.  ##
+    for i in range (0, n):
+        if(x[i+1] > limInferior and x[i+1] < limSuperior):
+            funcaoK = funcaoKs
+        else:
+            funcaoK = funcaoKa
+        diagB[i] = pow((1/h),2) * calculaIntegral(x[i], x[i+1], funcaoK) + pow((-1/h),2) * calculaIntegral(x[i+1], x[i+2], funcaoK) + pow((1/h),2) * calculaIntegral(x[i], x[i+1], "("+funcaoQ+")*pow((x-"+str(x[i])+"),2)") + pow((1/h),2) * calculaIntegral(x[i+1], x[i+2], "("+funcaoQ+")*pow(("+str(x[i+2])+"-x),2)")
+
+    ##  Calculo da diagonal acima da diagonal principal.  ##
+    for i in range (0, n-1):
+        if(x[i+1] > limInferior and x[i+1] < limSuperior):
+            funcaoK = funcaoKs
+        else:
+            funcaoK = funcaoKa
+        diagC[i] = -1 * pow((1/h),2) * calculaIntegral(x[i+1], x[i+2], funcaoK) + pow((1/h),2) * calculaIntegral(x[i+1],x[i+2], "("+funcaoQ+")*("+str(x[i+2])+"-x)*(x-"+str(x[i+1])+")")
+
+    ##  Calculo da diagonal abaixo da diagonal principal.  ##
+    for i in range (1, n):
+        if(x[i+1] > limInferior and x[i+1] < limSuperior):
+            funcaoK = funcaoKs
+        else:
+            funcaoK = funcaoKa
+        diagA[i] = -1 * pow((1/h),2) * calculaIntegral(x[i], x[i+1], funcaoK) + pow((1/h),2) * calculaIntegral(x[i],x[i+1], "("+funcaoQ+")*("+str(x[i+1])+"-x)*(x-"+str(x[i])+")")
+
+    ##  Imprime as três diagonais criadas acima.  ##
+    print("     Vetor a da matriz A:")
+    print(diagA)
+    print("     Vetor b da matriz A:")
+    print(diagB)
+    print("     Vetor c da matriz A:")
+    print(diagC)
+
+    ##  Calculo do vetor b, da funcao chapeu da equacao (8).  ##
+    b = np.zeros(n)
+    for i in range(0, n):
+        b[i] = calculaIntegral(x[i],x[i+1],"("+funcaoX+")*(x-"+str(x[i])+")/"+str(h)) + calculaIntegral(x[i+1],x[i+2],"("+funcaoX+")*("+str(x[i+2])+"-x)/"+str(h))
+
+    ##  Imprime o vetor b, calculado acima.  ##
+    print("     Vetor b do sistema: ")
+    print(b)
+
+    ##  Resolve sistema matricial para achar o vetor alfa.  ##
+    vetorAlfa = resolveTridiagonal(n,diagA,diagB,diagC,b)
+
+    ##  Imprime o vetor alfa calculado acima.  ##
+    print("     Vetor alfa, resolvido:")
+    print(vetorAlfa)
+
+    ##  Finalmente, calculo do valor aproximado.  ##
+    #vetoruExato      = np.zeros(n+2)
+    vetoruAproximado = np.zeros(n+2)
+
+    for j in range(0,n+2):
+        rAproximado = 0
+        for i in range(0, n):
+            if (x[j] <= x[i+1] and x[j] >= x[i]):
+                rAproximado = rAproximado + vetorAlfa[i] * (x[j] - x[i]) / h
+            elif (x[j] <= x[i+2] and x[j] >= x[i+1]):
+                rAproximado = rAproximado + vetorAlfa[i] * (x[i+2] - x[j]) / h
+        vetoruAproximado[j] = rAproximado
+
+    ##  Retorna vetor com os valores aproximados, nos pontos x_i.  ##
+    return vetoruAproximado
+
+
 def calculaMEF(n, L, funcaoX, funcaoK, funcaoQ):
     ##  Cálculo do intervalo entre pontos.  ##
     h = L/(n+1)
